b6a9a83a54e35acd03ac2c71060548419bc5a149
Lighton <laitonkamanga@gmail.com>
2026-02-01 21:27:11 +0200

    Quotesummision.js with CORS allow headers to match the app scripts in google sheet

--- js/quoteSubmission.js
+++ js/quoteSubmission.js
@@ -1,32 +1,33 @@
+const scriptURL = "https://script.google.com/macros/s/AKfycbxFvvNEnMFP4RRAwJOTqsb7SDcWesG_ZYyX2ww_CWX8Z3j-cyeU1YBoYQydF_nk55wxaw/exec";
 
-  const scriptURL = "https://script.google.com/macros/s/AKfycbwzjlvstBCVGiP8DEXXtuuCYYqdOBLKbeOTenk9OcXHHYolUKF05Zadi2x5eHbww048mQ/exec";
-
-  const form = document.getElementById("quoteForm");
+const form = document.getElementById("quoteForm");
 
 form.addEventListener("submit", e => {
   e.preventDefault();
 
-  const data = {
+  const formData = new URLSearchParams({
     name: form.name.value,
     email: form.email.value,
     phone: form.phone.value,
     service: form.service.value,
     message: form.message.value,
     budget: form.budget.value
-  };
+  });
 
   fetch(scriptURL, {
     method: "POST",
-    body: JSON.stringify(data),
     headers: {
-      "Content-Type": "application/json"
-    }
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: formData.toString()
   })
+  .then(res => res.json())
   .then(() => {
     alert("Quote submitted successfully!");
     form.reset();
   })
-  .catch(() => {
+  .catch(err => {
+    console.error(err);
     alert("Submission failed");
   });
 });
