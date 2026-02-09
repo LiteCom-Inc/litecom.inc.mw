7e331f68131e5feede9b09394660f7a6edc60b20
Lighton <laitonkamanga@gmail.com>
2026-02-09 12:05:48 +0200

    Using CORS library to fix the CORS error in quoteSubmission.js

--- js/quoteSubmission.js
+++ js/quoteSubmission.js
@@ -1,6 +1,10 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbwp1g8lw-P6fnvYSpjRhyZEQS3wSo46SSZfwDcJ3x-NLVQhGtOEpcv-IFOejDEJ6kQAiw/exec";
 
 const form = document.getElementById("quoteForm");
+const cors = require('cors');
+app.use(cors({
+  origin : "https://script.google.com/macros/s/AKfycbwp1g8lw-P6fnvYSpjRhyZEQS3wSo46SSZfwDcJ3x-NLVQhGtOEpcv-IFOejDEJ6kQAiw/exec",
+}))
 
 form.addEventListener("submit", e => {
   e.preventDefault();
